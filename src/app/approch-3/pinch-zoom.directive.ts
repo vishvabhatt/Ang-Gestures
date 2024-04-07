b1f998bb42787a73f8af5b8fc9d5cebb8453ce11
Vishva Bhatt <34930841+vishvabhatt@users.noreply.github.com>
2024-04-07 19:05:14 +0000

    discarded chanegs

--- src/app/approch-3/pinch-zoom.directive.ts
+++ src/app/approch-3/pinch-zoom.directive.ts
@@ -43,45 +43,7 @@ export class PinchZoomDirective implements AfterViewInit {
     hammerManager.add([pinch, swipe]);
     hammerManager.get('pinch').set({ enable: true });
 
-    hammerManager.add(new Hammer.Swipe({
-      direction: Hammer.DIRECTION_ALL,
-      threshold: 1,
-      velocity:0.1
-    })).recognizeWith(hammerManager.get('pan'));
-  
-    hammerManager.on('panstart panmove', (event) => { event.preventDefault(); });
-  
-    hammerManager.get('pinch').set({ enable: true });
-  
-    hammerManager.on("pinch", (event) => {
-      this.currentScale = this.adjustScale * event.scale;
-      this.currentDeltaX = this.adjustDeltaX + event.deltaX / this.currentScale;
-      this.currentDeltaY = this.adjustDeltaY + event.deltaY / this.currentScale;
-
-      let transforms = ['scale(' + this.currentScale + ')'];
-      transforms.push(
-        'translate(' + this.currentDeltaX + 'px,' + this.currentDeltaY + 'px)'
-      );
-      element.style.transform = transforms.join(' ');
-    });
-
-    hammerManager.on('pinchend', (event) => {
-      console.log('end event', event);
-      this.adjustScale = this.currentScale!;
-      this.adjustDeltaX = this.currentDeltaX!;
-      this.adjustDeltaY = this.currentDeltaY!;
-    });
-  
-    hammerManager.on("swipeleft", (event) =>  {
-      console.log('left-swipped', event);
-      this.nextImage();
-    });
-    hammerManager.on('swiperight',(event)=>{
-      console.log('right-swipped', event);
-      this.prevImage();
-    });
-
-    /*hammerManager.on('pinchmove', (event) => {
+    hammerManager.on('pinchmove', (event) => {
       console.log('pinchMove', event)
       this.currentScale = this.adjustScale * event.scale;
       this.currentDeltaX = this.adjustDeltaX + event.deltaX / this.currentScale;
@@ -108,25 +70,9 @@ export class PinchZoomDirective implements AfterViewInit {
     hammerManager.on('swiperight', (event) => {
       console.log('right-swipped', event);
       this.prevImage();
-    });*/
-    /*
-    element.addEventListener('touchmove',(event)=>{
-      console.log('----')
-      
-      if (event.touches.length === 2) {
-        if (event.touches[0].radiusX === event.touches[0].radiusY && event.touches[1].radiusX === event.touches[1].radiusY){
-          console.log('block pinch')
-          hammerManager.remove(pinch)
-        } else {
-          console.log('unblock')
-          hammerManager.add([pinch, swipe])
-        }
-      }
-      console.log('----')
-      
-    }) */
-   
+    });
   }
+  
   private nextImage(): void {
     if (this.currentIndex < this.images.length - 1) {
       this.currentIndex++;
