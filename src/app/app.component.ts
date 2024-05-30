24b12c552317171b4b6fb5c0592a92a1cd02e7f0
Vishva Bhatt <bhattvishva@ymail.com>
2024-05-30 17:08:15 +0530

    image change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  public imageUrl = '../assets/tay.jpg';
+  public imageUrl = '../assets/clouds.jpg';
   public eventOutput: string = '';
 
   constructor() {}
