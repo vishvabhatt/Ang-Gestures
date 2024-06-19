8d920c873559d62bea105dd2f4d5fff30094e97b
Vishva Bhatt <bhattvishva@ymail.com>
2024-06-19 11:20:44 +0000

    cleaned up

--- src/gesture/pinch-pan/pinch-pan-gesture.directive.ts
+++ src/gesture/pinch-pan/pinch-pan-gesture.directive.ts
@@ -22,20 +22,16 @@ export class PinchPanGestureDirective implements OnInit {
   private targetedElement: HTMLElement;
   private hammerManager: HammerManager;
   private isPinching = false;
-
   private adjustDeltaX = 0;
   private adjustDeltaY = 0;
   private adjustScale = 1;
-
   private currentDeltaX = 0;
   private currentDeltaY = 0;
   private currentScale = 1;
-  private imageWidth: number = 800;
-  private imageHeight: number = 600;
+  private imageWidth: number = 0;
+  private imageHeight: number = 0;
   private viewportWidth: number = 0;
   private viewportHeight: number = 0;
-  private initialScale = 1; // Store initial scale during pinchstart
-
   @Output() eventOutput = new EventEmitter<string>();
 
   constructor(
@@ -72,17 +68,10 @@ export class PinchPanGestureDirective implements OnInit {
   }
 
   private configureHammerManager() {
-    // const press = new Hammer.Pan(this.hammerConfigService.override.press);
-
-    const swipe = new Hammer.Swipe(this.hammerConfigService.override.swipe);
     const pinch = new Hammer.Pinch(this.hammerConfigService.override.pinch);
     const pan = new Hammer.Pan(this.hammerConfigService.override.pan);
-
     pan.requireFailure(pinch);
-    swipe.requireFailure(pinch);
-    swipe.requireFailure(pan);
-
-    this.hammerManager.add([pinch, pan, swipe]);
+    this.hammerManager.add([pinch, pan]);
   }
 
   private listenHammerCallbacks() {
@@ -93,44 +82,6 @@ export class PinchPanGestureDirective implements OnInit {
       this.isPinching = false;
     });
     this.hammerManager.on('panstart panmove', (event) => this.handlePan(event));
-    /*this.hammerManager.on('pinchstart pinchmove', (event) => {
-      this.isPinching = true;
-      console.log('pinchstart & move');
-      this.eventOutput.emit('pinchstart & move');
-      this.hammerManager.get('pan').set({ enable: false });
-      this.handlePinch(event);
-    });
-
-    this.hammerManager.on('pinchend pinchcancel', (event) => {
-      console.log('pinchend');
-      this.eventOutput.emit('pinchend');
-
-      this.isPinching = false;
-      this.handlePinch(event);
-
-      setTimeout(() => {
-        this.hammerManager.get('pan').set({ enable: true });
-      }, 10);
-    });
-
-    this.hammerManager.on('panstart panmove', (event) => {
-      if (!this.isPinching) {
-        this.isPinching = false;
-        console.log('panstart & move');
-        this.eventOutput.emit('panstart & move');
-        this.handlePan(event);
-      }
-    });
-
-    this.hammerManager.on('panend', (event) => {
-      if (!this.isPinching) {
-        this.isPinching = false;
-
-        console.log('panend');
-        this.eventOutput.emit('panend');
-        this.handlePan(event);
-      }
-    });*/
   }
 
   private handlePinch(event: any) {
@@ -143,17 +94,14 @@ export class PinchPanGestureDirective implements OnInit {
       this.currentScale = this.adjustScale * event.scale;
       this.currentDeltaX = this.adjustDeltaX + event.deltaX / this.currentScale;
       this.currentDeltaY = this.adjustDeltaY + event.deltaY / this.currentScale;
-
-      // Bounds checking for zooming
       const minScale = 0.5;
-      const maxScale = 2.0;
+      const maxScale = 5.0;
       this.currentScale = Math.max(
         minScale,
         Math.min(maxScale, this.currentScale)
       );
-
-      // Apply transformations using CSS
       const transforms = `scale(${this.currentScale}) translate(${this.currentDeltaX}px, ${this.currentDeltaY}px)`;
+      this.targetedElement.style.transition = 'transform 0.2s ease';
       this.targetedElement.style.transform = transforms;
     }
   }
@@ -162,17 +110,14 @@ export class PinchPanGestureDirective implements OnInit {
     if (!this.isPinching && event.type.includes('pan')) {
       this.currentDeltaX += event.deltaX;
       this.currentDeltaY += event.deltaY;
-
-      // Bounds checking for panning
       const maxX =
         (this.imageWidth * this.currentScale - this.viewportWidth) / 2;
       const maxY =
         (this.imageHeight * this.currentScale - this.viewportHeight) / 2;
       this.currentDeltaX = Math.max(-maxX, Math.min(maxX, this.currentDeltaX));
       this.currentDeltaY = Math.max(-maxY, Math.min(maxY, this.currentDeltaY));
-
-      // Apply transformations using CSS
       const transforms = `scale(${this.currentScale}) translate(${this.currentDeltaX}px, ${this.currentDeltaY}px)`;
+      this.targetedElement.style.transition = 'transform 0.2s ease';
       this.targetedElement.style.transform = transforms;
     }
   }
