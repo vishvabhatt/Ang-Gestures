39b52fab2f74b9aa6fc0df58d83ace1c90077c73
Vishva Bhatt <bhattvishva@ymail.com>
2024-10-09 19:56:02 +0000

    meesged up

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { AppModule } from './app/app.module';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import 'hammerjs';
 import 'zone.js';
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
